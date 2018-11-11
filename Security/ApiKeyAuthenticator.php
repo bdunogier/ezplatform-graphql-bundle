c271250f784de2f0d91877302be99147fec7e20f
Bertrand Dunogier <bertrand.dunogier@ez.no>
2018-11-12 00:47:36 +0100

    Implemented api key based authentication
    See doc/security.md
    
    Requires improvements.

---
+++ Security/ApiKeyAuthenticator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bdunogier
+ * Date: 11/11/2018
+ * Time: 22:10
+ */
+
+namespace BD\EzPlatformGraphQLBundle\Security;
+
+
+class ApiKeyAuthenticator
+{
+
+}
